56f11ad424a34a9f4762d00b93a6a2c7af234a96
Alan Soares <alansoares2014@gmail.com>
2023-10-21 13:30:52 -0300

    Docker deploy

---
+++ src/test/java/br/com/alansrsouza/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alansrsouza.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
